2fa465a72ecae6a87ef5f20cfc5ecd3dec7e452f
Smit Sabalpara <130885468+yuzikage@users.noreply.github.com>
2026-02-07 14:52:42 +0530

    Final App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import ProductTable  from './components/ProductTable';
 
 function App() {
@@ -13,33 +12,3 @@ function App() {
 }
 
 export default App
-
-    // useEffect(() => {
-    //   const observer = new IntersectionObserver(
-    //     (entries) => {
-    //       const firstEntry = entries[0];
-
-    //       if(firstEntry.isIntersecting && !loading) {
-    //         setSkip((prev) => prev + limit);
-    //       }
-    //     },
-
-    //     {
-    //       root: null,
-    //       rootMargin: "0px",
-    //       threshold: 1.0
-    //     }
-    //   );
-
-    //   const currentElement = loaderRef.current;
-
-    //   if(currentElement) {
-    //     observer.observe(currentElement);
-    //   }
-
-    //   return () => {
-    //     if(currentElement) {
-    //       observer.unobserve(currentElement);
-    //     }
-    //   };
-    // }, [loading]);
